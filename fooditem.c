276b88b67c349a1a6bf7752cfbadbfc3fdb98a7f
sakshimohan1320 <66312941+sakshimohan1320@users.noreply.github.com>
2020-06-07 23:30:29 +0530

    Created fooditem.c
    This file prints the food items and their prices on the screen

---
+++ fooditem.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+int i=3;
+switch(i)
+{
+case 1 :
+printf("Food item- Pizza \n Price- Rs 239");
+break;
+case 2 :
+printf("Food item- Burger \n Price- Rs 129");
+break;
+case 3 :
+printf("Food item- Pasta \n Price- Rs 179");
+break;
+case 4 :
+printf("Food item- French Fries \n Price- Rs 99");
+break;
+case 5 :
+printf("Food item- Sandwich \n Price- Rs 149");
+break;
+default :
+printf("number is not in the list");
+}
+return 0;
+}
